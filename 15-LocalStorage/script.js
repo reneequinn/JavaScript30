431d000ae597ff018ff69e05cabdbe1fefd75fcf
reneequinn <reneequinn17@gmail.com>
2019-12-16 19:18:48 +1030

    Completed additional challenges for day 15 and updated comments

--- 15-LocalStorage/script.js
+++ 15-LocalStorage/script.js
@@ -1,6 +1,9 @@
 // Getting elements
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
+const clearButton = document.querySelector('#clearList');
+const checkButton = document.querySelector('#checkAll');
+const uncheckButton = document.querySelector('#uncheckAll');
 
 // Array to hold all items as objects
 const items = JSON.parse(localStorage.getItem('items')) || [];
@@ -35,7 +38,30 @@ function toggleDone(e) {
   if (!e.target.matches('input')) return; // return if not an input
   const el = e.target;
   const index = el.dataset.index;
+  // Switches the done property to its opposite
   items[index].done = !items[index].done;
+  // Store in local storage with the updated done property
+  localStorage.setItem('items', JSON.stringify(items));
+  // Re-run populateList to visually update the page
+  populateList(items, itemsList);
+}
+
+// Additional Challenge Functions
+
+function clearList() {
+  localStorage.removeItem('items');
+  items.length = 0;
+  populateList(items, itemsList);
+}
+
+function checkAll() {
+  items.forEach(item => item.done = true);
+  localStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
+function uncheckAll() {
+  items.forEach(item => item.done = false);
   localStorage.setItem('items', JSON.stringify(items));
   populateList(items, itemsList);
 }
@@ -45,6 +71,11 @@ addItems.addEventListener('submit', addItem);
 
 // Listening for a click inside the item list
 itemsList.addEventListener('click', toggleDone);
+
+// Additional challenge event listeners
+clearButton.addEventListener('click', clearList);
+checkButton.addEventListener('click', checkAll);
+uncheckButton.addEventListener('click', uncheckAll);
 
 // Populate the list with items on page load
 populateList(items, itemsList);
